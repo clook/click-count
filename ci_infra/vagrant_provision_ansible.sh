05c47253fb265954e763ec5d554d90e22ccf8cb8
Olivier Cloirec <olivier.cloirec@free.fr>
2017-10-24 01:40:02 +0200

    Ansible Docker needs docker-py package

--- ci_infra/vagrant_provision_ansible.sh
+++ ci_infra/vagrant_provision_ansible.sh
@@ -4,6 +4,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 # Install ansible
 sudo apt-get update
-sudo apt-get install -y ansible
+sudo apt-get install -y ansible python-pip
+sudo pip install docker-py
 
 ansible --version
