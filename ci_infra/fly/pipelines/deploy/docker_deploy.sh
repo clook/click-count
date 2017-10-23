45d79603da8d1f554640ef1794bc1d56f146bd2d
Olivier Cloirec <olivier.cloirec@free.fr>
2017-10-23 23:28:43 +0200

    Make use of REDIS_HOST variable so far

--- ci_infra/fly/pipelines/deploy/docker_deploy.sh
+++ ci_infra/fly/pipelines/deploy/docker_deploy.sh
@@ -29,6 +29,6 @@ ansible-playbook deploy-app.yml \
 	-e env="${DEPLOY_ENV}" \
 	-e docker_tag="$docker_tag" \
 	-e host_name="$host_name" \
-	-e redis_host="192.168.33.10" \
+	-e redis_host="${REDIS_HOST}" \
 	--private-key ~/.ssh/id_rsa_app \
 	-i hosts/${DEPLOY_ENV} -u root
