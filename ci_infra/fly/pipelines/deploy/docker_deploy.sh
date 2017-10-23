d654704ab5478180ec5f17a1968a5edd4d2f925a
Olivier Cloirec <olivier.cloirec@free.fr>
2017-10-23 13:28:14 +0200

    Reformat SSH key from variable

--- ci_infra/fly/pipelines/deploy/docker_deploy.sh
+++ ci_infra/fly/pipelines/deploy/docker_deploy.sh
@@ -3,7 +3,8 @@
 set -e
 
 mkdir -p ~/.ssh/
-echo ${SSH_KEY} > ~/.ssh/id_rsa_app && chmod 600 ~/.ssh/id_rsa_app && cat ~/.ssh/id_rsa_app
+echo ${SSH_KEY} | sed -e 's/\(KEY-----\)\s/\1\n/g; s/\s\(-----END\)/\n\1/g' | sed -e '2s/\s\+/\n/g' > ~/.ssh/id_rsa_app
+chmod 600 ~/.ssh/id_rsa_app
 ansible-playbook deploy-app.yml -e env="${DEPLOY_ENV}" --private-key ~/.ssh/id_rsa_app -i hosts/${DEPLOY_ENV} -u root
 
 
