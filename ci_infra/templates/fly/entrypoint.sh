04f81b4272f7e3b4a0a77c032e47aae4ca84240f
Olivier Cloirec <olivier.cloirec@free.fr>
2017-10-23 13:23:30 +0200

    Multiple try over fly download if gateway not ready

--- ci_infra/templates/fly/entrypoint.sh
+++ ci_infra/templates/fly/entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
-curl '%CONCOURSE_URL%/api/v1/cli?arch=amd64&platform=linux' > /usr/local/bin/fly
+TRY_MAX=60
+
+try=0
+while ! curl '%CONCOURSE_URL%/api/v1/cli?arch=amd64&platform=linux' > /usr/local/bin/fly
+do
+	if [[ "$try" -gt "$TRY_MAX" ]]; then
+		echo "Can't download fly"
+		exit 1
+	fi
+	try=$((try + 1))
+	sleep 1
+done
+
 chmod +x /usr/local/bin/fly
 
 fly login -u concourse -p concourse -c %CONCOURSE_URL% -t xebia-test
