cc78c9e7eae825194344a46e0d8ac98abe374325
Olivier Cloirec <olivier.cloirec@free.fr>
2017-10-24 11:34:46 +0200

    Use logger instead of System.println

--- src/main/java/fr/xebia/clickcount/Configuration.java
+++ src/main/java/fr/xebia/clickcount/Configuration.java
@@ -1,10 +1,14 @@
 package fr.xebia.clickcount;
 
 import javax.inject.Singleton;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @Singleton
 public class Configuration {
 
+	private static final Logger log = LoggerFactory.getLogger(Configuration.class);
     public final String redisHost;
     public final int redisPort;
     public final int redisConnectionTimeout;  //milliseconds
@@ -12,10 +16,11 @@ public class Configuration {
     public Configuration() {
 		String envHost = System.getenv("HOST_REDIS");
 		redisHost = (envHost != null && !envHost.isEmpty())  ? envHost : "redis";
-		System.out.println("Redis server: " + redisHost);
+		log.info(">> Redis host: " + redisHost);
 
 		String envPort = System.getenv("PORT_REDIS");
 		redisPort = (envPort != null && !envPort.isEmpty())  ? Integer.valueOf(envPort) : 6379;
+		log.info(">> Redis port: " + redisPort);
 
         redisConnectionTimeout = 2000;
     }
